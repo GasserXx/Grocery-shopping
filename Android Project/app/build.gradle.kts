e9267e6bc79c8d529d429aadc904ebe5ae2f5dc0
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-12-05 12:09:00 +0200

    adding binding View || drenaming pngs to be imported

--- Android Project/app/build.gradle.kts
+++ Android Project/app/build.gradle.kts
@@ -25,6 +25,11 @@ android {
                 "proguard-rules.pro"
             )
         }
+
+
+    }
+    buildFeatures {
+        viewBinding = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
