77805cc7ee3b75db5a435e02411e6e76437faae7
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-11 19:34:14 +0200

    detail item kotlin file and add binding on it

---
+++ Android Project/app/src/main/java/com/umega/grocery/auth/fragments/DetailItemFragment.kt
@@ -0,0 +1,23 @@
+package com.umega.grocery.auth.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.umega.grocery.R
+import com.umega.grocery.databinding.DetailItemPageBinding
+
+
+class DetailItemFragment : Fragment() {
+    lateinit var binding : DetailItemPageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.detail_item_page,container,false)
+        return binding.root
+    }
+}
