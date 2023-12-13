30fd98669ec8210b8503e609fedf146f14c50a89
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-13 16:00:12 +0200

    create base kotlin file for ResultFragment.kt

---
+++ Android Project/app/src/main/java/com/umega/grocery/shopping/fragments/ResultFragment.kt
@@ -0,0 +1,23 @@
+package com.umega.grocery.shopping.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.umega.grocery.R
+import com.umega.grocery.databinding.DetailItemPageBinding
+import com.umega.grocery.databinding.ResultPageBinding
+
+class ResultFragment : Fragment() {
+    lateinit var binding : ResultPageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.result_page,container,false)
+        return binding.root
+    }
+}
