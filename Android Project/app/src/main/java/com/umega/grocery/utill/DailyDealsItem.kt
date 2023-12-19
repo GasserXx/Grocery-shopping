37354e57cc03f153abcfb186cb65cfe7eefde167
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-20 01:05:48 +0200

    dataClass of DailyDealsItem

---
+++ Android Project/app/src/main/java/com/umega/grocery/utill/DailyDealsItem.kt
@@ -0,0 +1,8 @@
+package com.umega.grocery.utill
+data class DailyDealsItem(
+    val itemName: String,
+    val priceAfterDiscount: Double,
+    val itemID: Int,
+    val isFavourite: Boolean,
+    val quantity: String
+)
