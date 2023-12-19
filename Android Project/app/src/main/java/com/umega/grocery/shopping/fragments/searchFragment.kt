3e3138e1e13bc26ae76d3ccf94c372146926acaf
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-19 18:12:13 +0200

    design search_page kotlin

---
+++ Android Project/app/src/main/java/com/umega/grocery/shopping/fragments/searchFragment.kt
@@ -0,0 +1,23 @@
+package com.umega.grocery.shopping.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.umega.grocery.R
+import com.umega.grocery.databinding.FilterPageBinding
+import com.umega.grocery.databinding.SearchPageBinding
+
+class Fragment : Fragment() {
+    lateinit var binding : SearchPageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.search_page,container,false)
+        return binding.root
+    }
+}
