7bd48ac2960696127cff55e2bc988cb173ff172c
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-24 17:15:44 +0200

    refresh categories and sub categories in local database , functions to visual categories and sub categories

--- Android Project/app/src/main/java/com/umega/grocery/dataBase/Repo.kt
+++ Android Project/app/src/main/java/com/umega/grocery/dataBase/Repo.kt
@@ -6,9 +6,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.umega.grocery.UserPreference
 import com.umega.grocery.dataBase.remote.Remote
+import com.umega.grocery.utill.Category
+import com.umega.grocery.utill.SubCategory
 import com.umega.grocery.utill.User
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 @OptIn(DelicateCoroutinesApi::class)
 class Repo(context: Context) {
@@ -32,5 +36,23 @@ class Repo(context: Context) {
             userPreference.storeUserID(remote.getUserID(userPreference.getEmail()))
         }
     }
+    // refresh categories and sub categories
+    suspend fun refreshCategoriesAndSubCategories(){
+        withContext(Dispatchers.IO) {
+            val allCategoriesFromRemote = remote.getCategories()
+            val allSubCategoriesFromRemote = remote.getSubCategories()
+            localDatabase.insertCategories(allCategoriesFromRemote)
+            localDatabase.insertSubCategories(allSubCategoriesFromRemote)
+        }
+    }
+    // get all categories  and subCategories
+    fun getAllCategories(categories:MutableLiveData<List<Category>>){
+        categories.value = localDatabase.getAllCategories()
+    }
+    fun getAllSubCategoriesByCategory(categoryId:Int,categories:MutableLiveData<List<SubCategory>>){
+        categories.value = localDatabase.getAllSubcategoriesByCategory(categoryId)
+    }
+    //TODO make function to get products from remote bt subCategory id
+
 
 }
