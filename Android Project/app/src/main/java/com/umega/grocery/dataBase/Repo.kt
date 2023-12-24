f410db5f5699693aa4f47738b673d92e4c3d5443
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-12-24 13:33:47 +0200

    making the 2 login supporting functions in the repo class

---
+++ Android Project/app/src/main/java/com/umega/grocery/dataBase/Repo.kt
@@ -0,0 +1,33 @@
+package com.umega.grocery.dataBase
+
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.umega.grocery.UserPreference
+import com.umega.grocery.dataBase.remote.Remote
+import com.umega.grocery.utill.User
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.runBlocking
+
+@OptIn(DelicateCoroutinesApi::class)
+class Repo {
+    private val remote = Remote()
+
+    fun authentication(user: User, response:MutableLiveData<Int>){
+        runBlocking {
+            response.value = remote.authentication(user)
+        }
+    }
+
+    fun registerUser(user: User, response:MutableLiveData<Int>){
+        runBlocking {
+            response.value = remote.registerUser(user)
+        }
+    }
+
+    fun storeUserID(userPreference: UserPreference){
+        runBlocking {
+            userPreference.storeUserID(remote.getUserID(userPreference.getEmail()))
+        }
+    }
+}
