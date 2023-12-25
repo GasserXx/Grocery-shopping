d4c7b6168f4dda0615bb140a4d693866a11ed645
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-12-25 20:27:18 +0200

    solving thread lock bug || adding functions to clear preference

--- Android Project/app/src/main/java/com/umega/grocery/UserPreference.kt
+++ Android Project/app/src/main/java/com/umega/grocery/UserPreference.kt
@@ -1,6 +1,7 @@
 package com.umega.grocery
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -9,6 +10,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 
 class UserPreference(private val context: Context) {
 
@@ -28,14 +30,11 @@ class UserPreference(private val context: Context) {
             preferences[userKey] ?: 0
         }
     suspend fun getUser(): Int {
-        var result = 0
-        userFlow.collect { value ->
-            result = value
-        }
-        return result
+        return userFlow.first()
     }
 
     suspend fun storeEmail(email:String) {
+        Log.i(TAG, "StoreEmail got called")
         context.dataStore.edit { settings ->
             settings[emailKey] = email
         }
@@ -45,10 +44,16 @@ class UserPreference(private val context: Context) {
             preferences[emailKey]
         }
     suspend fun getEmail(): String {
-        var result:String? = ""
-        emailFlow.collect { value ->
-            result = value
-        }
-        return if (result.isNullOrBlank())"" else result!!
+        Log.i(TAG, "emailFlow : $emailFlow")
+        val result:String? = emailFlow.first()
+        return if (result.isNullOrBlank()) "" else result
+    }
+
+    suspend fun clearPreference(){
+        storeUserID(-1)
+        storeEmail("")
+    }
+    companion object{
+        const val TAG = "USER PREFERENCE CLASS"
     }
 }
