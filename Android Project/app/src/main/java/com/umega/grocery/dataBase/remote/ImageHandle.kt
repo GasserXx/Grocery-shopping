7a3c143cfd4ba34cbf5a157f86cf7e2fb3c2d057
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-26 19:57:27 +0200

    class to handle image downloading from server and cache it

---
+++ Android Project/app/src/main/java/com/umega/grocery/dataBase/remote/ImageHandle.kt
@@ -0,0 +1,48 @@
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Environment
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class ImageHandle(private val context: Context) {
+    suspend fun cacheImage(imageUrl: String, customFileName: String): String? =
+        withContext(Dispatchers.IO) {
+            try {
+                val bitmap: Bitmap = Glide.with(context)
+                    .asBitmap()
+                    .load(imageUrl)
+                    .submit()
+                    .get()
+
+                val cacheDir =
+                    File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "products_cache")
+                if (!cacheDir.exists()) {
+                    cacheDir.mkdirs()
+                }
+                val customFile = File(cacheDir, customFileName)
+                val outputStream = FileOutputStream(customFile)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
+
+                return@withContext customFile.absolutePath
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return@withContext null
+            }
+        }
+    suspend fun getCachedFilePath(customFileName: String): String? =
+        withContext(Dispatchers.IO) {
+            val cacheDir =
+                File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "products_cache")
+            val customFile = File(cacheDir, customFileName)
+
+            return@withContext if (customFile.exists()) customFile.absolutePath else null
+        }
+}
+
+
