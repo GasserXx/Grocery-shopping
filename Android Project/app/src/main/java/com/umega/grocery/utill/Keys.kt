c6e8c1c1d1205207b19497b463a76d0ad1f45ce0
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-12-30 19:47:26 +0200

    creating a class for keys

---
+++ Android Project/app/src/main/java/com/umega/grocery/utill/Keys.kt
@@ -0,0 +1,9 @@
+package com.umega.grocery.utill
+
+
+class Keys{
+    companion object{
+        const val result_title_bundle_key = "title_string"
+        const val result_ids_bundle_key = "products_ids"
+    }
+}
