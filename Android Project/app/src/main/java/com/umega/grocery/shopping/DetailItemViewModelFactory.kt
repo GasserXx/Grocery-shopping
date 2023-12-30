52f86e7bfd9448b30c2f6972f80c29a89679c489
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-12-31 01:28:31 +0200

    handle recieve in  bundle and create view model

---
+++ Android Project/app/src/main/java/com/umega/grocery/shopping/DetailItemViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.umega.grocery.shopping
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+
+class DetailItemViewModelFactory (private val navController: NavController, private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailItemViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel(navController,context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
