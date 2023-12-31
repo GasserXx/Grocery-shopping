0a74ddd8a4b27bba5a56a8746c30daf6665927ee
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-12-31 11:36:51 +0200

    listening on logout flag

--- Android Project/app/src/main/java/com/umega/grocery/MainActivity.kt
+++ Android Project/app/src/main/java/com/umega/grocery/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.umega.grocery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -11,18 +12,36 @@ import com.umega.grocery.auth.LoginViewModel
 import com.umega.grocery.auth.LoginViewModelFactory
 import com.umega.grocery.dataBase.LocalDatabaseHelper
 import com.umega.grocery.databinding.ActivityMainBinding
+import com.umega.grocery.shopping.HomeViewModel
 import com.umega.grocery.shopping.HomeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController:NavController
+    private val viewModel: HomeViewModel by viewModels { HomeViewModelFactory(navController,this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        //handle logout
+        viewModel.logOut.observe(this){
+            if (it)
+                endActivity()
+        }
+
+    }
+    private fun endActivity(){
+        // Create an Intent to start the new activity
+        val intent = Intent(this, LoginActivity::class.java)
+        // Start the new activity
+        startActivity(intent)
+        // Finish the current activity to close it
+        finish()
     }
 }
